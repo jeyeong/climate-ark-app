b635b147e001d999c702a881b143c84ee3dc0cd8
Suzzat9 <shobithacherian@Shobithas-MacBook-Air.local>
2022-02-25 18:27:17 -0600

    added tiles

--- canvas/lib/main.dart
+++ canvas/lib/main.dart
@@ -1,52 +1,62 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MyApp()
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Climate Ark Canvas',
-      home: MyHomePage(
-        title: 'Janus (Canvas)',
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        centerTitle: true,
-        backgroundColor: const Color(0xFF09BC8A),
-      ),
-      body: Center(
-        child: Container(
-          margin: const EdgeInsets.all(16.0),
-          decoration: BoxDecoration(
-            border: Border.all(
-              width: 1.0,
-              color: const Color(0xFF000000),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Center(
+          child: Column(
+            children: <Widget>[
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              color: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
+                child: ListTile(leading: Icon(
+                    Icons.laptop,
+                    color: Colors.teal,
+                    ),
+                    title: 
+                    Text('Change laptop settings',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontSize: 15.0),
+                      ),
+                      ),
+              ),
             ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              color: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
+                child: ListTile(leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                      ),
+                      title: 
+                      Text('shobs@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 15.0),
+                        ),
+                        ),
+              ),
+            )],
           ),
         ),
       ),
-    );
+      );
   }
 }
+
+
