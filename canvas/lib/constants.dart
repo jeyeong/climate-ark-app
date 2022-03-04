f9533d86633f5fcd3843ae518dd2310cb4ae6b48
Vincent-Zhang4135 <65748334+Vincent-Zhang4135@users.noreply.github.com>
2022-03-04 12:52:00 -0600

    added more colors

--- canvas/lib/constants.dart
+++ canvas/lib/constants.dart
@@ -1,4 +1,33 @@
 import 'package:flutter/material.dart';
 
-const primaryColor = Color(0xFF09BC8A);
-const primaryLightColor = Color(0xFF9DE4D0);
+// For hex color usage, use this class
+/* For Eas
+class HexColor extends Color {
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll("#", "");
+    if (hexColor.length == 6) {
+      hexColor = "FF" + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+}
+*/
+
+// Primarys
+const primaryDarkestColor = Color(0xFF01251b);
+const primaryDarkerColor = Color(0xFF068360);
+const primaryDarkColor = Color(0xFF045e45);
+const primaryColor = Color(0xFF09bc8a);
+const primaryLightColor = Color(0xFF52d0ad);
+const primaryLighterColor = Color(0xFF84ddc4);
+const primaryLightestColor = Color(0xFFcdf1e7);
+
+// Darks
+const darkGreen = Color(0xFF004346);
+const primaryDark = Color(0xFF172A3A);
+
+// Whites
+const offsetWhite = Color(0xFFFAFAFA);
+const primaryWhite = Color(0xFFFFFFFF);
