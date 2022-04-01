1882dcda506078531b00c06de2a66e950770b9de
Unyoung Kim <uykim0005@gmail.com>
2022-04-01 15:10:20 -0500

    .

--- canvas/lib/main.dart
+++ canvas/lib/main.dart
@@ -18,7 +18,6 @@ class MyApp extends StatelessWidget {
           child: Padding(
             padding: EdgeInsets.all(15.0),
               child: MyStatefulWidget(),
-              
           ),
         ),
       ),
