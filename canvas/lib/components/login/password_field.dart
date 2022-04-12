c477cd8105c70514b0c0bb6a413d323c42d894c8
Je Yeong - Pycharm <jeyeong@uchicago.edu>
2022-04-12 00:44:34 -0500

    Added hero and revamped input boxes

--- canvas/lib/components/login/password_field.dart
+++ canvas/lib/components/login/password_field.dart
@@ -1,9 +1,12 @@
-// ignore_for_file: unnecessary_const
-import 'package:canvas/constants.dart';
 import 'package:flutter/material.dart';
 
 class PasswordField extends StatefulWidget {
-  const PasswordField({Key? key}) : super(key: key);
+  final TextEditingController controller;
+
+  const PasswordField({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
 
   @override
   _PasswordFieldState createState() => _PasswordFieldState();
@@ -13,10 +16,8 @@ class _PasswordFieldState extends State<PasswordField> {
   // Initially password is obscure
   bool _obscureText = true;
 
-  String? _password;
-
   // Toggles the password show status
-  void _toggle() {
+  void _togglePasswordView() {
     setState(() {
       _obscureText = !_obscureText;
     });
@@ -24,41 +25,63 @@ class _PasswordFieldState extends State<PasswordField> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            width: double.infinity,
-            decoration: BoxDecoration(
-                color: primaryLightestColor,
-                borderRadius: BorderRadius.circular(29)),
-            child: TextFormField(
-              cursorColor: primaryDarkColor,
-              // keyboardType: TextInputType.text,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                labelStyle: TextStyle(color: lightGrey),
-                border: InputBorder.none,
-                /*
-                icon: const Padding(
-                  padding: const EdgeInsets.only(top: 15.0),
-                  child: const Icon(Icons.lock),
-                ),*/
-              ),
-              onSaved: (val) => _password = val,
-              obscureText: _obscureText,
-            ),
+    return TextField(
+      controller: widget.controller,
+      obscureText: _obscureText,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        labelText: 'Password',
+        suffixIcon: IconButton(
+          icon: Icon(
+            _obscureText ? Icons.visibility : Icons.visibility_off,
           ),
-          TextButton(
-            onPressed: _toggle,
-            child: Text(
-              _obscureText ? "Show" : "Hide",
-              style: const TextStyle(color: primaryDarkestColor),
-            ),
-          )
-        ],
+          onPressed: _togglePasswordView,
+          hoverColor: const Color(0x00000000),
+          splashColor: const Color(0x00000000),
+        ),
       ),
     );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Container(
+  //     child: Column(
+  //       children: <Widget>[
+  //         Container(
+  //           padding: const EdgeInsets.symmetric(horizontal: 20),
+  //           width: double.infinity,
+  //           decoration: BoxDecoration(
+  //               color: primaryLightestColor,
+  //               borderRadius: BorderRadius.circular(29)),
+  //           child: TextFormField(
+  //             cursorColor: primaryDarkColor,
+  //             // keyboardType: TextInputType.text,
+  //             decoration: const InputDecoration(
+  //               labelText: 'Password',
+  //               labelStyle: TextStyle(color: lightGrey),
+  //               border: InputBorder.none,
+  //               /*
+  //               icon: const Padding(
+  //                 padding: const EdgeInsets.only(top: 15.0),
+  //                 child: const Icon(Icons.lock),
+  //               ),*/
+  //             ),
+  //             onSaved: (val) => _password = val,
+  //             obscureText: _obscureText,
+  //           ),
+  //         ),
+  //         TextButton(
+  //           onPressed: _toggle,
+  //           child: Text(
+  //             _obscureText ? "Show" : "Hide",
+  //             style: const TextStyle(color: primaryDarkestColor),
+  //           ),
+  //         )
+  //       ],
+  //     ),
+  //   );
+  // }
 }
--- canvas/lib/views/login_page.dart
+++ canvas/lib/views/login_page.dart
@@ -2,45 +2,61 @@ import 'package:canvas/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:canvas/components/login/login_button.dart';
 import 'package:canvas/components/general/input_field.dart';
-import 'package:canvas/components/general/logo.dart';
 import 'package:canvas/components/login/password_field.dart';
 import 'package:canvas/views/sign_up_page.dart';
 import 'package:canvas/views/forgot_pass_page.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Start listening to changes.
+    nameController.addListener(_printLatestValue);
+  }
+
+  void _printLatestValue() {
+    print('Second text field: ${nameController.text}');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          color: primaryWhite,
-          child: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 30),
+            color: primaryWhite,
             child: Column(
               children: [
-                // Part below app bar
-                Container(
-                  height: 15.0,
-                  width: double.infinity,
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.elliptical(12, 10),
-                      bottomRight: Radius.elliptical(12, 10),
-                    ),
-                    color: Color(0xff08b184),
-                  ),
-                ),
                 // Main content
                 SizedBox(
-                  width: 400.0,
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      const Logo(width: 200.0),
-                      const InputField(title: 'Username'),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 30, horizontal: 20),
+                        child: Image.asset('assets/hero.png'),
+                      ),
+                      InputField(
+                        title: 'Username',
+                        controller: nameController,
+                      ),
                       const SizedBox(height: 20.0),
-                      const PasswordField(),
+                      PasswordField(
+                        controller: passwordController,
+                      ),
                       const SizedBox(height: 20.0),
                       LoginButton(),
                       const SizedBox(height: 20.0),
