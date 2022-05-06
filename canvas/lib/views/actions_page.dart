0b4dc8b8136e333479d0a024397d9dcf6f751372
Unyoung Kim <uykim0005@gmail.com>
2022-05-06 16:29:29 -0500

    Filter Fixed

--- canvas/lib/views/actions_page.dart
+++ canvas/lib/views/actions_page.dart
@@ -12,7 +12,7 @@ class ActionsPage extends StatelessWidget {
       child: Stack(
         children: [
           Home_(),
-          MyStatefulWidget(),
+          // MyStatefulWidget(),
           SearchBarUI(),
           
         ],
