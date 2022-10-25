b3ca7caf6b0ae6ad84e030fb5f13f6203bc8c495
Maggie Zhao <maggiezhao_admin@Maggies-MacBook-Pro.local>
2022-10-24 23:36:34 -0500

    making our current work look more like the figma, still have to work on the history list side

---
+++ canvas/lib/components/impact_page/action_card.dart
@@ -0,0 +1,78 @@
+import 'package:canvas/constants.dart';
+import 'package:flutter/material.dart';
+
+class ActionCard extends StatefulWidget {
+  final int numActionsCompleted;
+  final double boxHeight;
+  final double boxWidth;
+  const ActionCard(
+      {required this.numActionsCompleted,
+      required this.boxHeight,
+      required this.boxWidth,
+      Key? key})
+      : super(key: key);
+
+  @override
+  _ActionCardState createState() => _ActionCardState(
+      numActionsCompleted: this.numActionsCompleted,
+      boxHeight: this.boxHeight,
+      boxWidth: boxWidth);
+}
+
+class _ActionCardState extends State<ActionCard> {
+  int numActionsCompleted;
+  double boxHeight;
+  double boxWidth;
+
+  _ActionCardState(
+      {required this.numActionsCompleted,
+      required this.boxHeight,
+      required this.boxWidth});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: boxWidth,
+      height: boxHeight,
+      padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+      margin: EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: primaryLightestColor,
+      ),
+      child: Row(children: [
+        Container(
+            child: Center(
+                child: Text(numActionsCompleted.toString(),
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: primaryWhite,
+                        fontFamily: 'Poppins'))),
+            width: boxHeight - 15,
+            height: boxHeight - 15,
+            margin: EdgeInsets.only(right: 10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: Color(0xff09bc8a),
+            )),
+        RichText(
+          text: TextSpan(
+            text: '',
+            children: <TextSpan>[
+              TextSpan(
+                  text: 'Actions Completed\n',
+                  style: TextStyle(
+                      fontSize: 15,
+                      color: Color(0xff09bc8a),
+                      fontFamily: 'Poppins')),
+              TextSpan(
+                  text: '+3 This Week',
+                  style: TextStyle(
+                      fontSize: 12, color: darkGrey, fontFamily: 'Poppins')),
+            ],
+          ),
+        ),
+      ]),
+    );
+  }
+}
--- canvas/lib/views/impact_page.dart
+++ canvas/lib/views/impact_page.dart
@@ -1,3 +1,5 @@
+import 'package:canvas/components/impact_page/action_card.dart';
+import 'package:canvas/components/impact_page/streak_action_card.dart';
 import 'package:flutter/material.dart';
 import 'package:canvas/components/impact_page/impact_box.dart';
 import 'package:canvas/components/impact_page/fun_fact.dart';
@@ -5,6 +7,7 @@ import 'package:canvas/constants.dart';
 import 'package:canvas/components/impact_page/leaf_fill.dart';
 import 'package:canvas/constants.dart';
 
+import '../components/impact_page/streak_card.dart';
 
 class ImpactPage extends StatelessWidget {
   const ImpactPage({Key? key}) : super(key: key);
@@ -12,58 +15,84 @@ class ImpactPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-      child: Center(
-      child: Container(
-        child: Column(
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height / 2 - 80,
-              decoration: BoxDecoration(
-                  color: Color(0xff08b184),
-                  //color: primaryWhite,
-                  //borderRadius: BorderRadius.circular(50),
-                  shape : BoxShape.rectangle,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(25),
-                    bottomRight: Radius.circular(25),
-                  ),
-                ),
-              child: Column(
-                children: [
-                  LeafBox(numSaved: 70, size: 100),
-                  Text.rich( TextSpan (
-                    text: '',
-                    children: <TextSpan>[
-                      TextSpan(text: '500 ', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: primaryWhite)),
-                      TextSpan(text: 'Kgs CO2', style: TextStyle(fontSize: 20, color: primaryWhite)),
-                    ],
-                  )),
-                  Text("Carbon Saved", style: TextStyle(
-                      fontSize: 20, color: primaryWhite,)), 
-                  FunFact(text: "Saving emissions worth a flight \nbetween Delhi and Bangalore", height: 70.0, icon: Icon(Icons.airplanemode_on), col: primaryLightColor), 
-              
-                ],
+        body: SafeArea(
+            child: Center(
+                child: Container(
+      child: Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 2 - 100,
+            decoration: BoxDecoration(
+              color: Color(0xff08b184),
+              //color: primaryWhite,
+              //borderRadius: BorderRadius.circular(50),
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(25),
+                bottomRight: Radius.circular(25),
               ),
             ),
-            Container(
-              child: Column(
-                children: [
-                  Container(margin: EdgeInsets.all(10), 
-                            width: MediaQuery.of(context).size.width, 
-                            height: 30, 
-                            color: Colors.red,
-                            child: Text("boxes")),
-                  ImpactBox()
-                ]
-              ),
+            child: Column(
+              children: [
+                LeafBox(numSaved: 70, size: 110),
+                Text.rich(TextSpan(
+                  text: '',
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: '500 ',
+                        style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: primaryWhite,
+                            fontFamily: "Poppins")),
+                    TextSpan(
+                        text: 'Kgs CO',
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: primaryWhite,
+                            fontFamily: "Poppins")),
+                    TextSpan(
+                        text: '2',
+                        style: TextStyle(
+                            fontSize: 10,
+                            color: primaryWhite,
+                            fontFamily: "Poppins")),
+                  ],
+                )),
+                Text("Carbon Saved",
+                    style: TextStyle(
+                        fontSize: 15,
+                        color: primaryWhite,
+                        fontFamily: "Poppins")),
+                FunFact(
+                    text:
+                        "Going great! You saved emissions worth\n a flight between Delhi and Banglore",
+                    height: 70.0,
+                    icon: Icon(Icons.airplanemode_on),
+                    col: primaryLighterColor),
+              ],
             ),
-          ],
-        ),                      
-      )
-      )
-      )
-    );
+          ),
+          Container(
+            child: Column(children: [
+              StreakActionCard(
+                streakDays: 7,
+                numActionsCompleted: 12,
+                boxHeight: 50,
+              ),
+
+              // Container(margin: EdgeInsets.all(10),
+              //           width: MediaQuery.of(context).size.width,
+              //           height: 30,
+              //           color: Colors.red,
+              //           child: Text("boxes")),
+
+              ImpactBox()
+            ]),
+          ),
+        ],
+      ),
+    ))));
   }
 }
