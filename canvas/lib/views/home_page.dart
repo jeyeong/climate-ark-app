7a0e55069690bc6f3533abf8778e189777712187
Je Yeong - Pycharm <jeyeong@uchicago.edu>
2022-10-31 14:56:02 -0500

    .

--- canvas/lib/views/home_page.dart
+++ canvas/lib/views/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:canvas/components/homepage/summary_card.dart';
 
+import 'package:canvas/data.dart';
+import 'package:canvas/utils/utils.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -9,16 +12,21 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<CarbonAction> completedActions =
+      getCompletedActions(fakeActions, fakeAccountData.actionsCompleted);
+
   @override
   Widget build(BuildContext context) {
+    int carbonSaved = calculateCarbonSaved(completedActions);
+
     return SingleChildScrollView(
         child: Column(
       children: [
         const SizedBox(height: 15),
-        const SummaryCard(
-          carbonSaved: "500",
-          streakDays: "7",
-          actionsCompleted: "12",
+        SummaryCard(
+          carbonSaved: carbonSaved.toString(),
+          streakDays: fakeAccountData.streak.toString(),
+          actionsCompleted: completedActions.length.toString(),
         ),
       ],
     ));
