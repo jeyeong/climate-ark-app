48e52a67e1e0825b32696783c1ec1492229445d9
Jaeyoung  Kim <jaeyoungkim@Jaeyoungs-MacBook-Pro.local>
2022-11-01 17:49:50 -0500

    maggie's proportional height thing

--- canvas/lib/views/action_page.dart
+++ canvas/lib/views/action_page.dart
@@ -4,6 +4,7 @@ import 'package:canvas/constants.dart';
 import 'package:canvas/list.dart';
 import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 import 'package:canvas/components/action_page/search_bar.dart';
+import 'package:canvas/components/action_page/actions_card.dart';
 
 class ActionPage extends StatelessWidget {
   const ActionPage({Key? key}) : super(key: key);
@@ -12,12 +13,32 @@ class ActionPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         //replace line 15 Stack with Column to see scrollable functionality
-        body: new Container(
-            color: primaryColor,
-            child: Column(children: [
-              //SearchBar(),
-              Positioned.fill(child: SearchBar()),
-              Positioned(top: 60, child: SuggestionBox()),
-            ])));
+        body: Column(children: [
+      Container(
+          height: MediaQuery.of(context).size.height * 0.15,
+          //height: 110,
+          width: double.infinity,
+          //color: Colors.transparent,
+          decoration: BoxDecoration(
+              color: primaryColor,
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10.0),
+                  bottomRight: Radius.circular(10.0))),
+          //SearchBar(),
+          child: Column(children: [
+            Positioned.fill(child: SearchBar()),
+            Positioned(top: 20, child: SuggestionBox()),
+            Positioned(child: ActionsCard()),
+          ]))
+    ]));
+    // body: new Container(
+    //     color: primaryColor,
+    //     child: Column(children: [
+    //       //SearchBar(),
+    //       Positioned.fill(child: SearchBar()),
+    //       Positioned(top: 60, child: SuggestionBox()),
+    //       Positioned(child: ActionsCard())
+    //     ]))
+    //     );
   }
 }
