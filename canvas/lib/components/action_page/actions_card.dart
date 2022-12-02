2f3ceab7d0555bf83c8e6cf575c15d4da77dc4c9
Iris Hwang <hwang@MacBook-Pro-3.local>
2022-12-01 18:11:09 -0600

    we are donegit add . yay... jim(mys)

--- canvas/lib/components/action_page/actions_card.dart
+++ canvas/lib/components/action_page/actions_card.dart
@@ -1,3 +1,4 @@
+import 'package:canvas/components/general/button.dart';
 import 'package:canvas/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
@@ -21,125 +22,109 @@ class _ActionsCardState extends State<ActionsCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 400,
       margin: const EdgeInsets.all(15.0),
       padding: const EdgeInsets.symmetric(horizontal: 5),
       decoration: BoxDecoration(
           border: Border.all(color: primaryColor),
           borderRadius: BorderRadius.all(Radius.circular(20))),
-      //child: Container(height: 50, width: 50, color: primaryColor),
-
-      //child: Column(
       child: Column(
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              //Padding(
-              //padding: const EdgeInsets.all(8.0),
               Expanded(
-                flex: 4,
+                flex: 5,
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: ClipRRect(
                     borderRadius: BorderRadius.circular(8.0),
                     child: Image.asset(
                       'assets/carpool.jpg',
-                      height: 200,
-                      width: 300,
                       fit: BoxFit.fill,
                     ),
                   ),
                 ),
               ),
-              //),
-              // Expanded, with its flex property, is used to "fill out the
-              // rest of the space" in the row.
               Expanded(
-                flex: 6,
+                flex: 4,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    // Flexible is used to wrap text when it is too long.
                     Container(
-                      margin: EdgeInsets.all(20),
+                      margin: EdgeInsets.all(10),
                       width: 300,
                       child: Text(
                         widget.action.actionName,
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 20,
-                          color: Colors.grey, // color of action header
+                          color: Colors.black, // color of action header
                         ),
                       ),
                     ),
                     Container(
-                      margin: EdgeInsets.all(20),
+                      margin: EdgeInsets.only(left: 10),
                       width: 300,
-                      child: Text(
-                        widget.action.actionDescription,
-                      ),
+                      child: Text(widget.action.actionDescription,
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.grey, // color of action header
+                          )),
                     ),
                   ],
                 ),
               ),
             ],
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Expanded(
-                flex: 65,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Expanded(
+                    flex: 7,
+                    child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16.0),
+                          color: primaryDarkColor,
+                        ),
+                        child: ListTile(
+                          leading: const Icon(Icons.check, color: primaryWhite),
+                          title: const Text('Completed',
+                              style: TextStyle(
+                                  color: primaryWhite,
+                                  fontWeight: FontWeight.bold)),
+                          onTap: () {},
+                        ))),
+                const Expanded(
+                    flex: 1,
+                    child: SizedBox(
+                      width: 10,
+                    )),
+                Expanded(
+                  flex: 3,
                   child: Container(
-                    height: 75,
-                    width: 400,
-                    decoration: const BoxDecoration(
-                      color: primaryColor,
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16.0),
+                      color: primaryLightestColor,
                     ),
-                    child: Text(
-                      "Completed",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.white, // color of action header
-                      ),
-                    ),
+                    padding: const EdgeInsets.all(12.0),
+                    child: Row(children: [
+                      Icon(Icons.energy_savings_leaf_rounded,
+                          color: primaryColor),
+                      Text(" 24",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: primaryColor)),
+                    ]),
                   ),
                 ),
-              ),
-              Expanded(
-                flex: 35,
-                child: Column(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.all(8.0),
-                      height: 60,
-                      width: 200,
-                      decoration: const BoxDecoration(
-                        color: primaryLightestColor,
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.all(8.0),
-                      height: 60,
-                      width: 200,
-                      decoration: const BoxDecoration(
-                        color: primaryDarkColor,
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
+              ],
+            ),
           ),
         ],
       ),
